7b3d5539db5e0c3a71fac671c1e2cf1ee8239f67
Jhonatta Tunales <zeroxcode78@gmail.com>
2020-11-21 22:31:35 -0600

    Feat: Movement on the line

--- js/typeSlide.js
+++ js/typeSlide.js
@@ -1,1 +1,39 @@
-alert("HOLA MUNDO");
+let pTS = {
+
+   btn: $$(".btn-type-slide"),
+   line: $(".line-type")
+
+}
+
+let mTS = {
+
+   start: () => {
+
+      pTS.btn.forEach( e => {
+
+         e,addEventListener(
+            'click',
+            mTS.moveSlide
+         );
+
+      });
+
+   },
+
+   moveSlide: e => {
+
+      if ( e.target == pTS.btn[0] ) {
+
+         pTS.line.style.left = "0%";
+
+      } else {
+
+         pTS.line.style.left = "50%";
+
+      }
+
+   }
+
+}
+
+mTS.start();
